a8bb64b5ecfe751a0c88a72eed8e581370808fb1
aman-barbaria <amanbarbaria@gmail.com>
2019-09-07 13:33:14 +0530

    browser cache headers set and error Msg change for twitter connect error

--- app.js
+++ app.js
@@ -140,6 +140,11 @@ const appendRequestDebugInfo = function(req, res, next) {
 };
 
 const setResponseHeader = async function(req, res, next) {
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Cache-Control', 'no-store, no-cache, max-age=0, must-revalidate, post-check=0, pre-check=0');
+  res.setHeader('Vary', '*');
+  res.setHeader('Expires', '-1');
+  res.setHeader('Last-Modified', new Date().toUTCString());
   next();
 };
 
