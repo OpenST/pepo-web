1a3d148b825120832d2993114b734a30774a2eee
Shraddha <shraddha.patil@ost.com>
2019-09-14 16:19:31 +0530

    inital redemption code

---
+++ assets/js/web/redemption/index.js
@@ -0,0 +1,13 @@
+;
+(function (window,$) {
+  var ost = ns('ost'),
+      redemptionPage  = ns('ost.redemptionPage');
+  var oThis = redemptionPage =  {
+    redemptionPagedata : null,
+
+    init : function (data) {
+      oThis.redemptionPagedata = data
+      console.log("redemptionPagedata",oThis.redemptionPagedata)
+    }
+  }
+})(window,jQuery)
