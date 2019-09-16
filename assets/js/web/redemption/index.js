990628d5b14161cfb3f529e0d70ff46f3dfb9575
thahir-reddy <thahir@ost.com>
2019-09-16 18:28:06 +0530

    redemption page ajax

--- assets/js/web/redemption/index.js
+++ assets/js/web/redemption/index.js
@@ -7,18 +7,41 @@
     init : function (data) {
       oThis.redemptionPagedata = data;
       oThis.productClick();
-      console.log("redemptionPagedata",oThis.redemptionPagedata)
+      $("#requestRedemption").on('click', function () {
+        oThis.requestAction();
+      });
+      // console.log("redemptionPagedata",oThis.redemptionPagedata)
     },
 
     productClick: function () {
       var productImg = $('.product-img');
+      var productDetail = $('.product-detail');
       productImg.on('click', function(){
-        var selfContainer = $(this).closest('.products');
-        selfContainer.hide();
-        $('.product-detail').fadeIn('slow');
+        $(this).closest('.products').hide();
+        productDetail.fadeIn('slow');
+        productDetail.find('.landscape-img').attr('src', $(this).data('src'));
+      })
+    },
+
+    requestAction: function () {
+      var requestRoute   = "/api/v1/redemptions";
+      var requestMethod  = "POST";
+      $.ajax({
+        url: requestRoute,
+        method: requestMethod,
+        success: function (response) {
+          if ( !response || !response.data || !response.data.success ) {
+
+          } else {
+
+          }
+        },
+        error: function() {
+
+        }
       })
     }
 
   }
 
-})(window,jQuery)
+})(window,jQuery);
