0dae33eaee3d66911e0340a0a73bd9f8c825ebd2
thahir-reddy <thahir@ost.com>
2019-09-16 16:39:58 +0530

    redemption page

--- assets/js/web/redemption/index.js
+++ assets/js/web/redemption/index.js
@@ -1,13 +1,24 @@
 ;
 (function (window,$) {
-  var ost = ns('ost'),
-      redemptionPage  = ns('ost.redemptionPage');
-  var oThis = redemptionPage =  {
+  var ost = ns('ost');
+  var oThis = ost.redemptionPage =  {
     redemptionPagedata : null,
 
     init : function (data) {
-      oThis.redemptionPagedata = data
+      oThis.redemptionPagedata = data;
+      oThis.productClick();
       console.log("redemptionPagedata",oThis.redemptionPagedata)
+    },
+
+    productClick: function () {
+      var productImg = $('.product-img');
+      productImg.on('click', function(){
+        var selfContainer = $(this).closest('.products');
+        selfContainer.hide();
+        $('.product-detail').fadeIn('slow');
+      })
     }
+
   }
+
 })(window,jQuery)
