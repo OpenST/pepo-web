d8adb9171840cc0e369968c9c46bdf4bd36be6f8
Kedar Chandrayan <kedarchandrayan@gmail.com>
2019-09-16 10:27:44 +0530

    redemptions v1

--- routes/redemptions.js
+++ routes/redemptions.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const rootPrefix = '..',
-  // GetRedemptionProduct = require(rootPrefix + '/app/services/GetRedemptionProduct'),
+  GetRedemptionProduct = require(rootPrefix + '/app/services/GetRedemptionProduct'),
   basicHelper = require(rootPrefix + '/helpers/basic'),
-  // cookieHelper = require(rootPrefix + '/helpers/cookie'),
+  cookieHelper = require(rootPrefix + '/helpers/cookie'),
   responseHelper = require(rootPrefix + '/lib/formatter/response'),
   sanitizer = require(rootPrefix + '/helpers/sanitizer'),
   renderResponseHelper = require(rootPrefix + '/helpers/renderResponseHelper');
@@ -14,32 +14,50 @@ const errorConfig = basicHelper.fetchErrorConfig();
 /* GET users account page. */
 router.get('/products', sanitizer.sanitizeDynamicUrlParams, async function (req, res, next) {
 
-  // let getRedemptionProductObj = new GetRedemptionProduct({headers: req.headers, decodedParams: req.decodedParams});
-  // let apiResponse = await getRedemptionProductObj.perform();
-  //
-  // cookieHelper.setNewCookies(req, res);
+  // Comment following 3 lines for local development of pepo-web. DO NOT COMMIT COMMENTED.
+  let getRedemptionProductObj = new GetRedemptionProduct({headers: req.headers, decodedParams: req.decodedParams});
+  let apiResponse = await getRedemptionProductObj.perform();
+  cookieHelper.setNewCookies(req, res);
 
-  let apiResponse = {
-    success: true,
-    data: {redemptionProducts: [
-        {
-          id: 1,
-          kind: "AMAZON",
-          status: "ACTIVE",
-          uts: parseInt(Date.now() / 1000)
-        },
-        {
-          id: 2,
-          kind: "STARBUCKS",
-          status: "ACTIVE",
-          uts: parseInt(Date.now() / 1000)
-        }
-      ]
-    }
-  };
+  // Un-comment only for local development of pepo-web. DO NOT COMMIT UNCOMMENTED.
+  // let apiResponse = {
+  //   success: true,
+  //   data: {redemption_products: [ { id: '1',
+  //       status: 'ACTIVE',
+  //       kind: 'AMAZON',
+  //       createdAt: 1568608359,
+  //       updatedAt: 1568608359 },
+  //       { id: '2',
+  //         status: 'ACTIVE',
+  //         kind: 'STARBUCKS',
+  //         createdAt: 1568608359,
+  //         updatedAt: 1568608359 },
+  //       { id: '3',
+  //         status: 'ACTIVE',
+  //         kind: 'NETFLIX',
+  //         createdAt: 1568608359,
+  //         updatedAt: 1568608359 },
+  //       { id: '4',
+  //         status: 'ACTIVE',
+  //         kind: 'AIRBNB',
+  //         createdAt: 1568608359,
+  //         updatedAt: 1568608359 },
+  //       { id: '5',
+  //         status: 'ACTIVE',
+  //         kind: 'CREATOR_PARTNERS',
+  //         createdAt: 1568608359,
+  //         updatedAt: 1568608359 },
+  //       { id: '6',
+  //         status: 'ACTIVE',
+  //         kind: 'UBER',
+  //         createdAt: 1568608359,
+  //         updatedAt: 1568608359 } ]
+  //   }
+  // };
+
 
   if (apiResponse.success) {
-    renderResponseHelper.renderWithLayout(req, res, 'loggedIn', 'web/_redemptionProduction', apiResponse.data);
+    renderResponseHelper.renderWithLayout(req, res, 'loggedIn', 'web/_redemptionProducts', apiResponse.data);
   } else {
     return responseHelper.renderApiResponse(apiResponse, res, errorConfig);
   }
