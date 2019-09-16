bc49581ec0c8881bb9d411eff928af9341add8d8
Kedar Chandrayan <kedarchandrayan@gmail.com>
2019-09-16 13:34:18 +0530

    redemptions v1

--- routes/redemptions.js
+++ routes/redemptions.js
@@ -15,47 +15,47 @@ const errorConfig = basicHelper.fetchErrorConfig();
 router.get('/products', sanitizer.sanitizeDynamicUrlParams, async function (req, res, next) {
 
   // Comment following 3 lines for local development of pepo-web. DO NOT COMMIT COMMENTED.
-  // let getRedemptionProductObj = new GetRedemptionProduct({headers: req.headers, decodedParams: req.decodedParams});
-  // let apiResponse = await getRedemptionProductObj.perform();
-  // cookieHelper.setNewCookies(req, res);
+  let getRedemptionProductObj = new GetRedemptionProduct({headers: req.headers, decodedParams: req.decodedParams});
+  let apiResponse = await getRedemptionProductObj.perform();
+  cookieHelper.setNewCookies(req, res);
 
   // Un-comment only for local development of pepo-web. DO NOT COMMIT UNCOMMENTED.
-  let apiResponse = {
-    success: true,
-    data: {
-      redemption_products: [ { id: '1',
-        status: 'ACTIVE',
-        kind: 'AMAZON',
-        createdAt: 1568608359,
-        updatedAt: 1568608359 },
-        { id: '2',
-          status: 'ACTIVE',
-          kind: 'STARBUCKS',
-          createdAt: 1568608359,
-          updatedAt: 1568608359 },
-        { id: '3',
-          status: 'ACTIVE',
-          kind: 'NETFLIX',
-          createdAt: 1568608359,
-          updatedAt: 1568608359 },
-        { id: '4',
-          status: 'ACTIVE',
-          kind: 'AIRBNB',
-          createdAt: 1568608359,
-          updatedAt: 1568608359 },
-        { id: '5',
-          status: 'ACTIVE',
-          kind: 'CREATOR_PARTNERS',
-          createdAt: 1568608359,
-          updatedAt: 1568608359 },
-        { id: '6',
-          status: 'ACTIVE',
-          kind: 'UBER',
-          createdAt: 1568608359,
-          updatedAt: 1568608359 } ],
-      balance: '1123000000000000'
-    }
-  };
+  // let apiResponse = {
+  //   success: true,
+  //   data: {
+  //     redemption_products: [ { id: '1',
+  //       status: 'ACTIVE',
+  //       kind: 'AMAZON',
+  //       createdAt: 1568608359,
+  //       updatedAt: 1568608359 },
+  //       { id: '2',
+  //         status: 'ACTIVE',
+  //         kind: 'STARBUCKS',
+  //         createdAt: 1568608359,
+  //         updatedAt: 1568608359 },
+  //       { id: '3',
+  //         status: 'ACTIVE',
+  //         kind: 'NETFLIX',
+  //         createdAt: 1568608359,
+  //         updatedAt: 1568608359 },
+  //       { id: '4',
+  //         status: 'ACTIVE',
+  //         kind: 'AIRBNB',
+  //         createdAt: 1568608359,
+  //         updatedAt: 1568608359 },
+  //       { id: '5',
+  //         status: 'ACTIVE',
+  //         kind: 'CREATOR_PARTNERS',
+  //         createdAt: 1568608359,
+  //         updatedAt: 1568608359 },
+  //       { id: '6',
+  //         status: 'ACTIVE',
+  //         kind: 'UBER',
+  //         createdAt: 1568608359,
+  //         updatedAt: 1568608359 } ],
+  //     balance: '1123000000000000'
+  //   }
+  // };
 
 
   if (apiResponse.success) {
