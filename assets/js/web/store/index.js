2bfea5b6d01ea41b05c17d2a6bf91e5a90834ce7
thahir-reddy <thahir@ost.com>
2019-10-16 14:05:13 +0530

    hide the input after showing the sucess message

--- assets/js/web/store/index.js
+++ assets/js/web/store/index.js
@@ -116,7 +116,7 @@
         success: function (response) {
           if ( response.success ) {
             $('#redemptionSuccess').show();
-            oThis.requestRedemptionBtn.hide();
+            oThis.requestSection.hide();
           } else {
             var errorData = response.err && response.err.error_data ,
                 errMsg;
