57b031139cf30adba7ce76ee2854c7b6fca5e27d
AnaghaMurtarkar <murtarkaranagha@gmail.com>
2020-02-24 14:04:59 +0530

    Language changes.

--- app/services/FireBaseUrl/Video.js
+++ app/services/FireBaseUrl/Video.js
@@ -83,7 +83,7 @@ class GetFirebaseVideoUrl extends FirebaseUrlBase {
     let urlParams = oThis._getFirebaseCommonUrlParams();
     Object.assign(urlParams, {
       link: oThis._fetchAppLaunchLink(),
-      sd: oThis.videoShareDetails.message ? oThis.videoShareDetails.message : 'Meet Your Peeps!\n' + 'REAL PEOPLE. REAL CONNECTIONS',
+      sd: oThis.videoShareDetails.message ? oThis.videoShareDetails.message : 'REAL PEOPLE. REAL CONNECTIONS.',
       si: 'https://d3attjoi5jlede.cloudfront.net/images/dynamic-link/artboard.png', // Some static image.
       ofl: oThis._fetchOflLink()
     });
