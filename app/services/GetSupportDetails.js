f47a4688a0353d33ebc3986e25b77eb6e6552ee0
shlokgilda <shlok@ost.com>
2020-03-05 20:15:57 +0530

    Removed loggers.

--- app/services/GetSupportDetails.js
+++ app/services/GetSupportDetails.js
@@ -48,8 +48,6 @@ class GetSupportDetails extends ServiceBase {
     let supportApiObj = new SupportApi(oThis.headers);
     let resp = await supportApiObj.validateSupportLink(oThis.decodedParams);
 
-    console.log('==resp.data====', resp.data);
-
     if (resp.isFailure()) {
       return Promise.reject(resp);
     } else {
