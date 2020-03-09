84aa1dde74d89c1ec7babbec884708cb4f05e269
AnaghaMurtarkar <murtarkaranagha@gmail.com>
2020-03-09 14:55:45 +0530

    Redirect url changes.

--- routes/webview/oAuth.js
+++ routes/webview/oAuth.js
@@ -3,16 +3,34 @@ const router = express.Router();
 
 const rootPrefix = '../..',
   pagePathConstants = require(rootPrefix + '/lib/globalConstant/pagePath'),
+  cookieConstants = require(rootPrefix + '/lib/globalConstant/cookie'),
   sanitizer = require(rootPrefix + '/helpers/sanitizer'),
   coreConstants = require(rootPrefix + '/config/coreConstants'),
   responseHelper = require(rootPrefix + '/lib/formatter/response'),
   renderResponseHelper = require(rootPrefix + '/helpers/renderResponseHelper');
+
+getRedirectPath = function(cookie) {
+  const decodedCookie = decodeURIComponent(cookie),
+    decodedValues = decodedCookie.split('&');
+
+  for(let i=0; i<decodedValues.length; i++) {
+    if(decodedValues[i].includes('rp=')) {
+      return '/' + decodedValues[i].replace('rp=', '');
+    }
+  }
+
+  return '/';
+};
 
 /* GET github oauth page. */
 router.get('/github/oauth', sanitizer.sanitizeDynamicUrlParams, async function (req, res, next) {
   let locals = {};
   if(req.decodedParams.code){
     locals = {oauth_response: {authorization_code: req.decodedParams.code}, oauth_kind: 'github'};
+  }
+
+  if(req.signedCookies[cookieConstants.loginRefererCookieName]) {
+    locals.redirect_url = getRedirectPath(req.signedCookies[cookieConstants.loginRefererCookieName]);
   }
   return renderResponseHelper.renderWithLayout(req, res, 'webView', 'web/_webView', locals);
 });
@@ -24,6 +42,10 @@ router.get('/twitter/oauth', sanitizer.sanitizeDynamicUrlParams, async function 
     locals = {oauth_response: {oauth_token: req.decodedParams.oauth_token,
         oauth_verifier: req.decodedParams.oauth_verifier}, oauth_kind: 'twitter'};
   }
+
+  if(req.signedCookies[cookieConstants.loginRefererCookieName]) {
+    locals.redirect_url = getRedirectPath(req.signedCookies[cookieConstants.loginRefererCookieName]);
+  }
   return renderResponseHelper.renderWithLayout(req, res, 'webView', 'web/_webView', locals);
 });
 
@@ -33,6 +55,11 @@ router.get('/google/oauth', sanitizer.sanitizeDynamicUrlParams, async function (
   if(req.decodedParams.code){
     locals = {oauth_response: {authorization_code: req.decodedParams.code}, oauth_kind: 'google'};
   }
+
+  if(req.signedCookies[cookieConstants.loginRefererCookieName]) {
+    locals.redirect_url = getRedirectPath(req.signedCookies[cookieConstants.loginRefererCookieName]);
+  }
+
   return renderResponseHelper.renderWithLayout(req, res, 'webView', 'web/_webView', locals);
 });
 
@@ -43,6 +70,11 @@ router.post('/apple/oauth', sanitizer.sanitizeDynamicUrlParams, async function (
     locals = {oauth_response: {authorization_code: req.decodedParams.code, identity_token: req.decodedParams.id_token},
               oauth_kind: 'apple'};
   }
+
+  if(req.signedCookies[cookieConstants.loginRefererCookieName]) {
+    locals.redirect_url = getRedirectPath(req.signedCookies[cookieConstants.loginRefererCookieName]);
+  }
+
   return renderResponseHelper.renderWithLayout(req, res, 'webView', 'web/_webView', locals);
 });
 
