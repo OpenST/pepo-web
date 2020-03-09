8884d35f565b1038dc8441bd654b399b6982e838
AnaghaMurtarkar <murtarkaranagha@gmail.com>
2020-03-09 17:44:25 +0530

    minor changes.

--- routes/webview/oAuth.js
+++ routes/webview/oAuth.js
@@ -16,7 +16,7 @@ getRedirectPath = function(state) {
     if (state) {
       const decodedState = JSON.parse(base64Helper.decode(state));
       // Added slash to stop unwanted redirects (security fix)
-      redirectPath = decodedState.rd ? '/' + decodedState.rd : null;
+      redirectPath = decodedState.hasOwnProperty('rd') ? '/' + decodedState.rd : null;
     }
   } catch (e) {
     // Logger
