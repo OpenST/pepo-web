6d2c8db15f8983b4f013005dd1beb2bf32113764
Ashutosh <ashutosh@ost.com>
2020-03-12 19:30:07 +0530

    Merge branch 'browser_sdk_integration' of github.com:ostdotcom/pepo-web into browser_sdk_integration

--- routes/pepo/webRouteHelper.js
+++ routes/pepo/webRouteHelper.js
@@ -20,25 +20,21 @@ const UserApi = require(rootPrefix + '/lib/pepoApi/User'),
   async perform(request, response, layout, contentPartialPath, locals, callback) {
     const oThis = this;
 
+    let currentUserDetails;
+
     locals = locals || {};
     locals.apiResponseData = locals.apiResponseData || {};
-
-    console.log('==111111========locals====-===================', locals);
 
     if(!locals.apiResponseData.current_user_data && request.headers && cookieConstants.hasWebLoginCookie(request.headers['cookie'])){
       let currentUserData = await oThis.getCurrentUserData(request);
       if(currentUserData.data){
         locals.apiResponseData.current_user_data = currentUserData.data;
+        currentUserDetails = locals.apiResponseData && locals.apiResponseData.current_user_data;
       }
     }
-    console.log('==222222========locals====-===================', locals);
-
-    let currentUserDetails = locals.apiResponseData && locals.apiResponseData.data
-                             && locals.apiResponseData.data.current_user_data;
-
-    console.log('======currentUserDetails/n/n/n', locals.apiResponseData, "/n/n/n", locals.apiResponseData.data,"/n/n/n", locals.apiResponseData.data.current_user_data );
 
     locals.currentUser = new CurrentUser(currentUserDetails);
+    console.log('==222222========locals====-===================', locals);
 
     renderResponseHelper.renderWithLayout(request, response, layout, contentPartialPath, locals, callback);
 
