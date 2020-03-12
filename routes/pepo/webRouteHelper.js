a390711174e90df8de9abb9e9d667b8a4b3e6375
Ashutosh <ashutosh@ost.com>
2020-03-12 19:32:05 +0530

    Resolve conflicts

--- routes/pepo/webRouteHelper.js
+++ routes/pepo/webRouteHelper.js
@@ -20,8 +20,6 @@ const UserApi = require(rootPrefix + '/lib/pepoApi/User'),
   async perform(request, response, layout, contentPartialPath, locals, callback) {
     const oThis = this;
 
-    let currentUserDetails;
-
     locals = locals || {};
     locals.apiResponseData = locals.apiResponseData || {};
 
@@ -29,12 +27,11 @@ const UserApi = require(rootPrefix + '/lib/pepoApi/User'),
       let currentUserData = await oThis.getCurrentUserData(request);
       if(currentUserData.data){
         locals.apiResponseData.current_user_data = currentUserData.data;
-        currentUserDetails = locals.apiResponseData && locals.apiResponseData.current_user_data;
       }
     }
-
+    let currentUserDetails = locals.apiResponseData && locals.apiResponseData.current_user_data;
     locals.currentUser = new CurrentUser(currentUserDetails);
-    console.log('==222222========locals====-===================', locals);
+    console.log('==222222========locals=======================', locals);
 
     renderResponseHelper.renderWithLayout(request, response, layout, contentPartialPath, locals, callback);
 
