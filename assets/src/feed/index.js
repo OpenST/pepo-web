c98b88396c741488de0aa93654bca3b8c2dba545
Mayur Patil <mayurupatil@gmail.com>
2020-03-13 18:22:46 +0530

    feed modal

--- assets/src/feed/index.js
+++ assets/src/feed/index.js
@@ -12,35 +12,68 @@ import deepGet from 'lodash/get';
 class Feed {
 
   constructor(){
+    this.currentItemIndex = null;
+    this.simpleDataTable = null;
   }
 
   init = (config) => {
-    this.bindEvents();
+    // sequence is important.
     this.fetchFeed();
+    this.bindEvents();
   };
 
   bindEvents = () => {
     const jFeedParent = $('#feedParent'),
-      jFeedParentWrapper = $("#feedModalWrapper");
+      jFeedModalWrapper = $("#feedModal .modal-dialog .modal-content .feedContainer");
+
+
       jFeedParent.on('click', '.feedList', (e) => {
-      let videoId = $(e.currentTarget).data('video-id');
-      const feedModal = ejs.compile(feedItemModal, { client : true });
-      const jFeedModal = $(feedModal({
-        videoId,
-        DataGetters
-      }));
-        jFeedParentWrapper.html( '');
-        jFeedParentWrapper.append(jFeedModal);
+        this.currentItemIndex = $(e.currentTarget).data('result-index');
+        this.modalUIUpdate();
         $('#feedModal').modal('show');
         e.preventDefault();
         e.stopPropagation();
     });
+
+    jFeedModalWrapper.on('click', '.next-video', (e) => {
+      this.currentItemIndex += 1;
+      this.modalUIUpdate();
+
+    });
+
+
+    jFeedModalWrapper.on('click', '.prev-video', (e) => {
+      if(this.currentItemIndex >= 1){
+        this.currentItemIndex -= 1;
+        this.modalUIUpdate();
+      }
+    });
+
+  };
+
+
+  modalUIUpdate = () => {
+    const jFeedParentWrapper = $("#feedModal .modal-dialog .modal-content .feedContainer");
+    let videoId = deepGet(this.getCurrentIndexResult(), 'video_id');
+    const feedModal = ejs.compile(feedItemModal, { client : true });
+    const jFeedModal = $(feedModal({
+      videoId,
+      DataGetters
+    }));
+    //jFeedParentWrapper.html( '');
+    jFeedParentWrapper.html(jFeedModal);
+  };
+
+
+  getCurrentIndexResult = () => {
+    let results = this.simpleDataTable.getResults();
+    return deepGet(results[this.currentItemIndex], 'payload');
   };
 
 
   fetchFeed = ()=> {
     const oThis =  this;
-    const simpleDataTable = new SimpleDataTable({
+    this.simpleDataTable = new SimpleDataTable({
       jParent: $("#feedParent"),
       fetchResultsUrl: "/api/web/feeds",
       rowTemplate: ejs.compile(feedItemList, {client: true}),
