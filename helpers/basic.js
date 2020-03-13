f2e4376b3bce7a96de314c8b08fc750e348b5b1a
alpeshvmodi <alpeshvmodi@gmail.com>
2020-03-13 23:38:59 +0530

    channel integration

--- helpers/basic.js
+++ helpers/basic.js
@@ -164,7 +164,7 @@ class BasicHelper {
 
   getIncludesReplaceText(kind){
     if(kind == 'tags'){
-      return '$1<span>$2</span>';
+      return '$1<span><b>$2</b></span>';
     } else if(kind == 'links') {
       return '$1<a href="$2" rel="noopener noreferrer nofollow" target="_blank">$2</a>';
     } else {
