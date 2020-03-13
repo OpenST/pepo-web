746caef2e06bbf68826fe2a431b111c0d01a7f20
Ashutosh <ashutosh@ost.com>
2020-03-13 21:54:45 +0530

    Channel done

--- assets/src/channel.js
+++ assets/src/channel.js
@@ -1,13 +1,13 @@
 import  ns from "../js/libs/namespace";
 import BaseView from "../src/common/BaseView";
 import videoList from "./common/video/videoList";
+import  deepGet from "lodash/get"
 
 class Channel extends BaseView {
 
   constructor(config){
     super(config);
-    console.log("this.config",  this.config);
-    const channelId = "";
+    const channelId = deepGet(this.config,  "apiResponse.channel.id");
     videoList.init({fetchApi:  `/api/web/channels/${channelId}/videos` });
   }
 
