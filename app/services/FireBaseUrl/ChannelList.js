62b7cf7ad52edf57271b09b7ca0edb4bfc43e1b8
kedarchandrayan <kedarchandrayan@gmail.com>
2020-03-19 16:25:18 +0530

    communities

---
+++ app/services/FireBaseUrl/ChannelList.js
@@ -0,0 +1,104 @@
+const rootPrefix = '../../..',
+  FirebaseUrlBase = require(rootPrefix + '/app/services/FireBaseUrl/Base'),
+  pagePathConstants = require(rootPrefix + '/lib/globalConstant/pagePath'),
+  responseHelper = require(rootPrefix + '/lib/formatter/response'),
+  coreConstants = require(rootPrefix + '/config/coreConstants'),
+  ChannelShareDetails = require(rootPrefix + '/lib/pepoApi/Channel');
+
+/**
+ * Class to get firebase redirection url for channel url
+ *
+ */
+class GetFirebaseChannelUrl extends FirebaseUrlBase {
+  constructor(params) {
+    super(params);
+
+    const oThis = this;
+    oThis.permalink = oThis.decodedParams.permalink;
+    oThis.channelShareDetails = params.channelShareDetails;
+  }
+
+  /**
+   * Async perform
+   *
+   * @returns {Promise<*>}
+   * @private
+   */
+  async _asyncPerform() {
+    const oThis = this;
+
+    const url = oThis._generateFireBaseUrl();
+
+    return responseHelper.successWithData({
+      url: url,
+      pageMeta: {
+        title: oThis.urlParams.st,
+        description: oThis.urlParams.sd,
+        robots: 'index, follow',
+        canonical: oThis._channelBaseUrl(),
+        og: {
+          title: oThis.urlParams.st,
+          description: oThis.urlParams.sd,
+          image: oThis.urlParams.si,
+          url: oThis._fetchAppLaunchLink()
+        },
+        twitter: {
+          title: oThis.urlParams.st,
+          description: oThis.urlParams.sd,
+          image: oThis.urlParams.si,
+          card: "summary_large_image"
+        }
+      }
+    });
+  }
+
+  /**
+   * Get firebase url params
+   *
+   * @returns {Object}
+   * @private
+   */
+  _getFirebaseUrlParams(){
+    const oThis = this;
+
+    let urlParams = oThis._getFirebaseCommonUrlParams();
+    Object.assign(urlParams, {
+      link: oThis._fetchAppLaunchLink(),
+      st: oThis.channelShareDetails.title || '',
+      sd: oThis.channelShareDetails.description ? oThis.channelShareDetails.description : coreConstants.DEFAULT_SHARE_DESCRIPTION,
+      si: oThis.channelShareDetails.poster_image_url ? oThis.channelShareDetails.poster_image_url : coreConstants.DEFAULT_SHARE_IMAGE,
+      ofl: oThis._fetchOflLink()
+    });
+    // Assign all url params
+    return urlParams;
+  }
+
+  /**
+   * Fetch app launch link
+   * @returns {*}
+   * @private
+   */
+  _fetchAppLaunchLink() {
+    const oThis = this;
+
+    let baseLink = oThis._channelBaseUrl();
+    let queryString = oThis._generateUtmQueryString();
+
+    return baseLink + (queryString ? `?${queryString}` : '');
+  }
+
+  /**
+   * Channel base url
+   *
+   * @returns {string}
+   * @private
+   */
+  _channelListBaseUrl() {
+    const oThis = this;
+
+    return `${coreConstants.PEPO_DOMAIN}${pagePathConstants.communities}/${oThis.permalink}`;
+  }
+
+}
+
+module.exports = GetFirebaseChannelUrl;
---
+++ app/services/GetChannelList.js
@@ -0,0 +1,189 @@
+const rootPrefix = '../..',
+  ServiceBase = require(rootPrefix + '/app/services/Base'),
+  GetFirebaseChannelUrl = require(rootPrefix + '/app/services/FireBaseUrl/Channel'),
+  responseHelper = require(rootPrefix + '/lib/formatter/response'),
+  logger = require(rootPrefix + '/lib/logger/customConsoleLogger'),
+  appUpdateLinksConstants = require(rootPrefix + '/lib/globalConstant/appUpdateLinks'),
+  ChannelLib = require(rootPrefix + '/lib/pepoApi/Channel'),
+  basicHelper = require(rootPrefix + '/helpers/basic');
+
+/**
+ * Class for Getting channel
+ *
+ * @class GetChannel
+ */
+class GetChannel extends ServiceBase {
+  /**
+   * Constructor
+   *
+   * @augments ServiceBase
+   *
+   * @constructor
+   */
+  constructor(params) {
+    super(params);
+
+    const oThis = this;
+    oThis.headers = params.headers || {};
+    oThis.decodedParams = params.decodedParams;
+    oThis.permalink = oThis.decodedParams.permalink;
+
+    oThis.apiResponseData = {};
+  }
+
+  /**
+   * Perform: Perform async
+   *
+   * @return {Promise<void>}
+   */
+  async _asyncPerform() {
+    const oThis = this;
+
+    await oThis._validateAndSanitize();
+
+    await oThis._convertDescriptionForDisplay();
+
+    await oThis._parseTexts();
+
+    await oThis.getCurrentUser();
+
+    return oThis._prepareResponse();
+
+  }
+
+  /**
+   *
+   * @returns {Promise<void>}
+   * @private
+   */
+  async _validateAndSanitize() {
+  }
+
+  /**
+   * Fetch channel
+   *
+   * @return {Promise<Result>}
+   * @private
+   */
+  async _fetchChannel() {
+    const oThis = this;
+    logger.log('Start::_fetchChannel');
+
+    const serviceResp = await new ChannelLib(oThis.headers).getChannelDetails({permalink: oThis.permalink});
+    if (serviceResp.isFailure()) {
+      return Promise.reject(serviceResp);
+    }
+    oThis.apiResponseData = serviceResp.data;
+
+    let pageMetaResponse = await new GetFirebaseChannelUrl({
+      headers: oThis.headers,
+      decodedParams: oThis.decodedParams,
+      channelShareDetails: {
+        title: oThis._getChannelName() + ' - Pepo',
+        description: oThis._getDescription(),
+        poster_image_url: oThis._getChannelImageUrl()
+      }
+    }).perform();
+
+    if (pageMetaResponse.isFailure()) {
+      return Promise.reject(pageMetaResponse);
+    }
+
+    if (pageMetaResponse.success) {
+      oThis.apiResponseData.firebase_channel_url = pageMetaResponse.data.url;
+      oThis.apiResponseData.share_url = pageMetaResponse.data.pageMeta.canonical;
+      oThis.apiResponseData.page_meta = pageMetaResponse.data.pageMeta;
+    }
+
+    return responseHelper.successWithData({});
+  }
+
+  _getChannelName() {
+    const oThis = this;
+    return oThis.apiResponseData['channel'].name;
+  }
+
+  _getDescription() {
+    const oThis = this,
+      channelId = oThis.apiResponseData['channel'].id,
+      channelDetails = oThis.apiResponseData['channel_details'][channelId],
+      descriptionId = channelDetails['description_id'];
+
+    if(!descriptionId || !oThis.apiResponseData['texts'] || !oThis.apiResponseData['texts'][descriptionId]){
+      return '';
+    }
+
+    return oThis.apiResponseData['texts'][descriptionId]['text'];
+  }
+
+  _getChannelImageUrl() {
+    const oThis = this,
+      channelId = oThis.apiResponseData['channel'].id,
+      channelDetails = oThis.apiResponseData['channel_details'][channelId],
+      coverImageId = channelDetails['cover_image_id'],
+      imageResolutions = oThis.apiResponseData['images'][coverImageId]['resolutions'];
+
+    return imageResolutions['original']['url'];
+  }
+
+  async _convertDescriptionForDisplay() {
+    const oThis = this,
+      maxPositionToSplit = 120,
+      channelId = oThis.apiResponseData['channel'].id,
+      channelDetails = oThis.apiResponseData['channel_details'][channelId],
+      descriptionId = channelDetails['description_id'];
+
+    if(!descriptionId || !oThis.apiResponseData['texts'] || !oThis.apiResponseData['texts'][descriptionId]){
+      return;
+    }
+
+    const description = oThis.apiResponseData['texts'][descriptionId]['text'];
+    if(maxPositionToSplit > description.length){
+      return;
+    }
+
+    const splitablePosition = description.lastIndexOf(' ', maxPositionToSplit);
+
+    oThis.apiResponseData['texts'][descriptionId]['text'] = description.slice(0, splitablePosition) +
+      '<span class="showMore">... Show More</span><span class="afterText">' + description.slice(splitablePosition) + '</span>'
+  }
+
+  /**
+   *
+   * @returns {Promise<void>}
+   * @private
+   */
+  async _parseTexts() {
+    const oThis = this,
+      texts = oThis.apiResponseData.texts;
+
+    for(let textId in texts){
+      let textDetails = texts[textId];
+      textDetails.convertedText = basicHelper.replaceIncludesinText(textDetails.text, textDetails.includes);
+    }
+
+  }
+
+  /**
+   *
+   * @returns {Promise<*|result>}
+   * @private
+   */
+  async _prepareResponse() {
+    const oThis = this;
+
+    return responseHelper.successWithData({
+      apiResponseData: oThis.apiResponseData,
+      androidAppLink: appUpdateLinksConstants.androidUpdateLink,
+      iosAppLink: appUpdateLinksConstants.iosUpdateLink,
+      pageMeta: oThis.apiResponseData.page_meta,
+      firebaseUrls: {openInApp: oThis.apiResponseData.firebase_channel_url},
+      showFooter: false,
+      currentUserData: oThis.currentUserData,
+      currentUser: oThis.currentUser
+    })
+  }
+
+}
+
+module.exports = GetChannel;
