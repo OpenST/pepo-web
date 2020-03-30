94c602dd1423b279cca9213d4e486e9c603cecda
Ashutosh <ashutosh@ost.com>
2020-03-30 16:10:27 +0530

    CSRF code fix

--- assets/src/services/ZoomMeeting.js
+++ assets/src/services/ZoomMeeting.js
@@ -78,43 +78,44 @@ class ZoomMeeting {
     ) {
       return false;
     }
+    
     return true;
   }
 
-  setZoomMeetingUserName (channel, meeting ,  onBeforeSend , onSuccess , onError, onComplete ){
-    if(!channel || !meeting) return ;
-    //TODO SHOW modal
-    //TODO Validate name
-    $.ajax({
-      url: "",//TODO
-      method:'POST',
-      beforeSend: ()=> {
-        //TODO Disabled butoon
-        //TODO Change btn  text
-        onBeforeSend && onBeforeSend();
-      },
-      success: ( response )=>{
-        if(response && response.success ){
-          //TODO Close modal and navigate to meeting page
-          window.location = `/communities/${channel.permalink}/meetings/${meeting}`;
-          onSuccess && onSuccess(response);
-        }else {
-          //TODO SHOW error in modal
-          onError && onError(response);
-        }
-      },
-      error : ( xhr,status,error )=>{
-        //SHOW error in modal
-        onError && onError(error);
-      },
-      complete : () => {
-        //TODO enable butoon
-        //TODO Change btn text to normal
-        onComplete && onComplete();
-      }
-    });
-
-  }
+  // setZoomMeetingUserName (channel, meeting ,  onBeforeSend , onSuccess , onError, onComplete ){
+  //   if(!channel || !meeting) return ;
+  //   //TODO SHOW modal
+  //   //TODO Validate name
+  //   $.ajax({
+  //     url: "",//TODO
+  //     method:'POST',
+  //     beforeSend: ()=> {
+  //       //TODO Disabled butoon
+  //       //TODO Change btn  text
+  //       onBeforeSend && onBeforeSend();
+  //     },
+  //     success: ( response )=>{
+  //       if(response && response.success ){
+  //         //TODO Close modal and navigate to meeting page
+  //         window.location = `/communities/${channel.permalink}/meetings/${meeting}`;
+  //         onSuccess && onSuccess(response);
+  //       }else {
+  //         //TODO SHOW error in modal
+  //         onError && onError(response);
+  //       }
+  //     },
+  //     error : ( xhr,status,error )=>{
+  //       //SHOW error in modal
+  //       onError && onError(error);
+  //     },
+  //     complete : () => {
+  //       //TODO enable butoon
+  //       //TODO Change btn text to normal
+  //       onComplete && onComplete();
+  //     }
+  //   });
+  //
+  // }
 
 
 
--- assets/src/zoomMeeting.js
+++ assets/src/zoomMeeting.js
@@ -1,11 +1,42 @@
 const { ZoomMtg } = window;
+import  ns from "../src/libs/namespace";
+
+import * as ajaxHooks from './utils/ajaxHooks';
 
 class ZoomMeeting {
 
-    constructor(){
+    constructor(config){
+        
+        if(config){
+          if(typeof config.apiResponse == "string"){
+            config.apiResponse =  JSON.parse( config.apiResponse );
+          }
+          this.config = config;
+          
+          if(this.config.goto){
+            this.initGoTo();
+            return;
+          }
+        }
+        
         ZoomMtg.setZoomJSLib('https://source.zoom.us/1.7.2/lib', '/av');
         ZoomMtg.preLoadWasm();
         ZoomMtg.prepareJssdk();
+    }
+    
+    initGoTo(){
+      if(!this.config.permalink || !this.config.meetingId){
+        window.parent.location = this.config.goto;
+      }
+      $.ajax({
+        url: `/api/web/channels/${this.config.permalink}/meetings/${this.config.meetingId}`,
+        method:'POST',
+        success: function (res) {},
+        error : function( xhr,status,error ){},
+        complete : function()  {
+          window.parent.location = this.config.goto;
+        }
+      });
     }
 
     init(options, onSuccess, onError){
@@ -43,5 +74,8 @@ class ZoomMeeting {
     }
 
 }
+
+const pepo = ns("pepo");
+pepo.zoomMeeting = ZoomMeeting;
 
 window.ZoomMeeting = ZoomMeeting;
