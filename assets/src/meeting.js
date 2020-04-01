067ee9ee056e2c19bd92c046d8a0e2beed54600e
kedarchandrayan <kedarchandrayan@gmail.com>
2020-04-01 17:38:18 +0530

    err change

--- assets/src/meeting.js
+++ assets/src/meeting.js
@@ -275,6 +275,12 @@ class Meeting extends BaseView {
   }
 
   showError(message) {
+    message = message || '';
+
+    if((message).toLowerCase() == 'meeting is not started') {
+      message = 'Pepo live event has not started';
+    }
+
     this.jqIframe.hide();
     this.jqLoader.hide();
     this.jqError.find('.error-text').html(message);
